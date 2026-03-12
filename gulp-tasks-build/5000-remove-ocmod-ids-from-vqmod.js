bbc5b0e5a81f05628f56e84cebaf95c64dc8d1c5
redochka <reda.abdi@gmail.com>
2026-03-12 14:31:02 +0100

    add existence checks for source folders in `copy-doc-folder` and `remove-ocmod-ids-from-vqmod` tasks because newer gulp fails if given src does not exist

--- gulp-tasks-build/5000-remove-ocmod-ids-from-vqmod.js
+++ gulp-tasks-build/5000-remove-ocmod-ids-from-vqmod.js
@@ -1,43 +1,49 @@
+const path = require("path");
 module.exports = function (extension) {
 
-    const gulp    = require('gulp');
-    const cheerio = require('gulp-cheerio');
-    const cu      = require("../gulp-includes/common-utils.js");
-    const fs      = require('fs-extra');
+  const gulp = require('gulp');
+  const cheerio = require('gulp-cheerio');
+  const cu = require("../gulp-includes/common-utils.js");
+  const fs = require('fs-extra');
 
-    /**
+  /**
+   *
+   */
+  //gulp.task('remove-ocmod-ids-from-vqmod', gulp.series('translate-markdown-to-html-from-dist', function () {
+  gulp.task('remove-ocmod-ids-from-vqmod', function (done) {
+
+    const FINAL_FILENAME = cu.getPublicNameOfDelivery(extension);
+    const LEGACY_DIST_DIR = cu.getPathToExtensionLegacyDistFolder(FINAL_FILENAME, extension);
+
+    /*
      *
      */
-    //gulp.task('remove-ocmod-ids-from-vqmod', gulp.series('translate-markdown-to-html-from-dist', function () {
-    gulp.task('remove-ocmod-ids-from-vqmod', function () {
+    fs.removeSync(path.join(LEGACY_DIST_DIR, '/ocmod-only'));
 
-        const FINAL_FILENAME  = cu.getPublicNameOfDelivery(extension);
-        const LEGACY_DIST_DIR = cu.getPathToExtensionLegacyDistFolder(FINAL_FILENAME, extension);
+    /*
+     *
+     */
+    let VQMOD_XML_PATH = path.join(LEGACY_DIST_DIR, "/upload/vqmod/xml");
 
-        /*
-         *
-         */
-        fs.removeSync(LEGACY_DIST_DIR + '/ocmod-only');
+    // Check if the source directory exists
+    if (!fs.existsSync(VQMOD_XML_PATH)) {
+      console.log("★ vqmod/xml folder not found, skipping ocmod processing.");
+      done();
+      return;
+    }
 
-        /*
-         *
-         */
-        // return gulp.src([LEGACY_DIST_DIR + "/upload/vqmod/**/*"])
-        return gulp.src([LEGACY_DIST_DIR + "/upload/vqmod/xml/*"])
+    return gulp.src([path.join(VQMOD_XML_PATH, "/*")])
+      .pipe(cheerio({  //remove ocmod tags from vqmod files.
+        run          : function ($, file) {
+          $('code').remove();
+          $('name').remove();
+          $('link').remove();
+        },
+        parserOptions: {
+          xmlMode: true
+        }
+      }))
+      .pipe(gulp.dest(VQMOD_XML_PATH));
 
-            .pipe(cheerio({  //remove ocmod tags from vqmod files.
-                run          : function ($, file) {
-                    $('code').remove();
-                    $('name').remove();
-                    $('link').remove();
-                },
-                parserOptions: {
-                    xmlMode: true
-                }
-
-            }))
-            // .pipe(gulp.dest(LEGACY_DIST_DIR + "/upload/vqmod"));
-            .pipe(gulp.dest(LEGACY_DIST_DIR + "/upload/vqmod/xml"));
-
-    });
+  });
 }
--- gulp-tasks-build/5200-task-copy-doc-folder.js
+++ gulp-tasks-build/5200-task-copy-doc-folder.js
@@ -1,19 +1,31 @@
-module.exports = function(extension){
+const fs = require("fs");
+const path = require("path");
+module.exports = function (extension) {
 
-    const gulp    = require('gulp');
-    const cu      = require("../gulp-includes/common-utils.js");
+  const gulp = require('gulp');
+  const cu = require("../gulp-includes/common-utils.js");
 
 
-    gulp.task('copy-doc-folder', function (done) {
+  gulp.task('copy-doc-folder', function (done) {
 
-        const FINAL_FILENAME  = cu.getPublicNameOfDelivery(extension);
-        const MY_DIST_DIR     = cu.getPathToExtensionDistFolder(FINAL_FILENAME, extension);
-        const MY_BUILD_DIR    = cu.getPathToExtensionBuildFolder(FINAL_FILENAME, extension);
-        const LEGACY_DIST_DIR = cu.getPathToExtensionLegacyDistFolder(FINAL_FILENAME, extension);
+    const FINAL_FILENAME = cu.getPublicNameOfDelivery(extension);
+    const MY_DIST_DIR = cu.getPathToExtensionDistFolder(FINAL_FILENAME, extension);
+    const MY_BUILD_DIR = cu.getPathToExtensionBuildFolder(FINAL_FILENAME, extension);
+    const LEGACY_DIST_DIR = cu.getPathToExtensionLegacyDistFolder(FINAL_FILENAME, extension);
 
-        console.log("★ Going to copy doc folder");
-        return gulp.src([MY_BUILD_DIR + "/public/doc/**"])
-            .pipe(gulp.dest(MY_DIST_DIR + "/doc"));
-    });
+    console.log("★ Going to copy doc folder");
+
+    const DOC_SRC = path.join(MY_BUILD_DIR, "/public/doc");
+
+    // Check if the source directory exists
+    if (!fs.existsSync(DOC_SRC)) {
+      console.log("★ Doc folder not found, skipping copy.");
+      done();
+      return;
+    }
+
+    return gulp.src([path.join(DOC_SRC, "/**")])
+      .pipe(gulp.dest(path.join(MY_DIST_DIR, "/doc")));
+  });
 
 }
